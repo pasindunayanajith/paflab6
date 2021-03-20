ed5fc685632c03b470e55f55423dcceb50ef1322
“pasindunayanajith” <pasindunayanjith98@gmail.com>
2021-03-21 01:05:22 +0530

    paflab6

---
+++ Lab6Rest/src/com/ItemService.java
@@ -0,0 +1,56 @@
+package com;
+import model.Item;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+@Path("/Items")
+public class ItemService
+{
+ Item itemObj = new Item();
+@GET
+@Path("/")
+@Produces(MediaType.TEXT_HTML)
+public String readItems()
+ {
+	 return itemObj.readItems(); 
+ }
+@POST
+@Path("/")
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+@Produces(MediaType.TEXT_PLAIN)
+public String insertItem(@FormParam("itemCode") String itemCode,
+ @FormParam("itemName") String itemName,
+ @FormParam("itemPrice") String itemPrice,
+ @FormParam("itemDesc") String itemDesc)
+{
+ String output = itemObj.insertItem(itemCode, itemName, itemPrice, itemDesc);
+return output;
+}
+
+
+@PUT
+@Path("/")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.TEXT_PLAIN)
+public String updateItem(String itemData)
+{
+//Convert the input string to a JSON object
+ JsonObject itemObject = new JsonParser().parse(itemData).getAsJsonObject();
+//Read the values from the JSON object
+ String itemID = itemObject.get("itemID").getAsString();
+ String itemCode = itemObject.get("itemCode").getAsString();
+ String itemName = itemObject.get("itemName").getAsString();
+ String itemPrice = itemObject.get("itemPrice").getAsString();
+ String itemDesc = itemObject.get("itemDesc").getAsString();
+ String output = itemObj.updateItem(itemID, itemCode, itemName, itemPrice, itemDesc);
+return output;
+}
+
+}
+
